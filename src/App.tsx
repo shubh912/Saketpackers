20de12df6ef75a4746e185b1145f9b8b65192d5f
shubh912 <57699783+shubh912@users.noreply.github.com>
2026-08-25 07:23:04 +0000

    Remove floating WhatsApp button

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MobileCTABar from './components/MobileCTABar';
-import WhatsAppFloat from './components/WhatsAppFloat';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -71,7 +70,6 @@ function SiteChrome() {
       </main>
       <Footer />
       <MobileCTABar />
-      <WhatsAppFloat />
     </div>
   );
 }
